b1dbd60e5e0943916fe5dba3769bd43c0a361f48
Chenchen Zheng <imcc@Chenchens-MBP.lan>
2019-08-25 01:27:52 -0700

    fix redux addItem

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,26 @@
 import React from 'react'
 import AppNavbar from './components/AppNavBar'
 import ShoppingList from './components/ShoppingList'
+import ItemModel from './components/ItemModel'
+import { Container } from 'reactstrap'
+
+import { Provider } from 'react-redux'
+import store from './store'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <AppNavbar />
-      <ShoppingList />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <AppNavbar />
+        <Container>
+          <ItemModel />
+          <ShoppingList />
+        </Container>
+      </div>
+    </Provider>
   );
 }
 
