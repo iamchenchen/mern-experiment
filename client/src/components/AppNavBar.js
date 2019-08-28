87f7acbbc3ae0b7c09fdec4df9e1480a0660426d
Chenchen Zheng <imcc@Chenchens-MBP.lan>
2019-08-27 23:52:06 -0700

    add final login feature

--- client/src/components/AppNavBar.js
+++ client/src/components/AppNavBar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import {
   Collapse,
   Navbar,
@@ -10,13 +10,17 @@ import {
 } from 'reactstrap'
 import RegisterModal from './auth/RegisterModal'
 import Logout from './auth/Logout'
+import LoginModal from './auth/LoginModal'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 
 class AppNavbar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isOpen: false
-    }
+  state = {
+    isOpen: false
+  }
+
+  static propTypes = {
+    auth: PropTypes.object.isRequired
   }
 
   toggle = () => {
@@ -26,6 +30,31 @@ class AppNavbar extends Component {
   }
 
   render() {
+    const { isAuthenticated, user } = this.props.auth
+
+    const authLinks = (
+      <Fragment>
+        <NavItem>
+          <span className="navbar-text mr-3">
+            <strong>{ user? `Welcome ${user.name}` : '' }</strong>
+          </span>
+        </NavItem>
+        <NavItem>
+          <Logout />
+        </NavItem>  
+      </Fragment>
+    )
+
+    const guestLinks = (
+      <Fragment>
+        <NavItem>
+          <RegisterModal />
+        </NavItem>
+        <NavItem>
+          <LoginModal />
+        </NavItem>
+      </Fragment>
+    )
     return (
       <div>
         <Navbar color="dark" expend="sm" className="mb-5">
@@ -34,12 +63,7 @@ class AppNavbar extends Component {
             <NavbarToggler onClick={this.toggle} />
             <Collapse isOpen={this.state.isOpen} navbar>
               <Nav className="ml-auto" navbar>
-                <NavItem>
-                  <RegisterModal />
-                </NavItem>
-                <NavItem>
-                  <Logout />
-                </NavItem>
+                { isAuthenticated ? authLinks : guestLinks }
               </Nav>
             </Collapse>
           </Container>
@@ -49,4 +73,8 @@ class AppNavbar extends Component {
   }
 }
 
-export default AppNavbar
+const mapStateToProps = state => ({
+  auth: state.auth
+})
+
+export default connect(mapStateToProps, null)(AppNavbar)
--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -6,6 +6,11 @@ import { getItems, deleteItem } from '../actions/itemActions'
 import PropTypes from 'prop-types'
 
 class ShoppingList extends Component {
+  static propTypes = {
+    getItems: PropTypes.func.isRequired,
+    item: PropTypes.object.isRequired,
+    isAuthenticated: PropTypes.bool
+  }
 
   componentDidMount() {
     this.props.getItems()
@@ -16,7 +21,7 @@ class ShoppingList extends Component {
   }
 
   render() {
-    const { items } = this.props.item
+    const { items, isAuthenticated } = this.props.item
     return (
       <Container>
         <ListGroup>
@@ -28,12 +33,12 @@ class ShoppingList extends Component {
                   classNames="fade"
                   timeout={500}>
                     <ListGroupItem>
-                      <Button 
+                      { isAuthenticated ?                       <Button 
                         className="remove-btn"
                         color="danger"
                         size="sm"
                         onClick={this.onDeleteClick.bind(this, _id)}
-                      >&times;</Button>
+                      >&times;</Button> : null }
                       {name}
                     </ListGroupItem>
                   </CSSTransition>
@@ -46,15 +51,9 @@ class ShoppingList extends Component {
   }
 }
 
-
-
-ShoppingList.propTypes = {
-  getItems: PropTypes.func.isRequired,
-  item: PropTypes.object.isRequired
-}
-
 const mapStateToProps = (state) => ({
-  item: state.item
+  item: state.item,
+  isAuthenticated: state.isAuthenticated
 })
 
 export default connect(mapStateToProps, { getItems, deleteItem })(ShoppingList)
